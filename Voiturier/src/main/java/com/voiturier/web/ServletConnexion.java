878c9bb5894d84afa4b5db0c4f0a7c4c0b578e87
Aston <Aston@Aston-Pc>
2015-09-23 12:54:07 +0200

    Creation package Web pour les servlets

--- Voiturier/src/main/java/com/voiturier/web/ServletConnexion.java
+++ Voiturier/src/main/java/com/voiturier/web/ServletConnexion.java
@@ -1,6 +1,8 @@
 package com.voiturier.web;
 
 import java.io.IOException;
+
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,27 +14,33 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ServletConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletConnexion() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletConnexion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
+	@Override
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String login = request.getParameter("inLogin");
+		String pwd = request.getParameter("inPass");
 	}
 
 }
