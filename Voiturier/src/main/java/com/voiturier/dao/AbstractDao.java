b046e811e70cdc7074f7bd35914685ce55fc09fd
aston <aston@aston-PC>
2015-09-23 16:26:09 +0200

    ab

--- Voiturier/src/main/java/com/voiturier/dao/AbstractDao.java
+++ Voiturier/src/main/java/com/voiturier/dao/AbstractDao.java
@@ -4,12 +4,15 @@
 package com.voiturier.dao;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
+import com.voiturier.dao.ex.ExceptionDao;
 import com.voiturier.dao.util.AbstractJdbcMapper;
 import com.voiturier.entity.IEntity;
 
@@ -68,7 +71,37 @@ public abstract class AbstractDao<T extends IEntity> implements Serializable, ID
 	public abstract T select(Object pUneClef) throws com.voiturier.dao.ex.ExceptionDao;
 
 	@Override
-	public abstract List<T> selectAll(String pAWhere, String pAnOrderBy) throws com.voiturier.dao.ex.ExceptionDao;
+	public  List<T> selectAll(String pAWhere, String pAnOrderBy) throws com.voiturier.dao.ex.ExceptionDao{
+		List<T> result = new ArrayList<>();
+
+		try {
+
+			StringBuffer request = new StringBuffer();
+			request.append("select ").append(this.getAllColumnNames()).append(" from ");
+			request.append(this.getTableName());
+			if (pAWhere != null) {
+				request.append(" where ");
+				request.append(pAWhere);
+			}
+			if (pAnOrderBy != null) {
+				request.append(" order by ");
+				request.append(pAnOrderBy);
+			}
+			request.append(';');
+			if (this.LOG.isDebugEnabled()) {
+				this.LOG.debug("Requete: " + request.toString());
+			}
+
+			result = this.getJdbcTemplate().query(request.toString(), (ResultSetExtractor<List<T>>)this.getMapper());
+
+		} catch (Throwable e) {
+			throw new ExceptionDao(e);
+		} finally {
+
+		}
+
+		return result;
+	}
 
 	protected abstract AbstractJdbcMapper<T> getMapper();
 
