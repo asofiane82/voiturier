38646f855eac0c18ef9e696d3131dae7adf3a71d
Aston <Aston@Aston-Pc>
2015-09-23 15:34:59 +0200

    p

--- Voiturier/src/main/java/com/voiturier/service/IAuthentificationService.java
+++ Voiturier/src/main/java/com/voiturier/service/IAuthentificationService.java
@@ -3,10 +3,31 @@
  */
 package com.voiturier.service;
 
+import com.voiturier.entity.IUtilisateurEntity;
+import com.voiturier.service.ex.AuthentificationException;
+import com.voiturier.service.ex.ErreurTechniqueException;
+
 /**
  * @author Aston
  *
  */
 public interface IAuthentificationService {
+	/**
+	 * Authentifie un utilisateur.
+	 *
+	 * @param pLogin
+	 *            le login
+	 * @param pPassword
+	 *            le password
+	 * @return l'utilisateur authentifie, leve une exception sinon
+	 * @throws AuthentificationException
+	 *             si un probleme survient
+	 * @throws ErreurTechniqueException
+	 *             si un probleme survient
+	 * @throws NullPointerException
+	 *             avec comme message le nom du parametre null
+	 */
+	public abstract IUtilisateurEntity authentifier(String pLogin, String pPassword)
+			throws AuthentificationException, ErreurTechniqueException;
 
 }
--- Voiturier/src/main/java/com/voiturier/web/ServletConnexion.java
+++ Voiturier/src/main/java/com/voiturier/web/ServletConnexion.java
@@ -5,15 +5,26 @@ import java.io.IOException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.voiturier.entity.IUtilisateurEntity;
+import com.voiturier.service.IAuthentificationService;
+
 /**
  * Servlet implementation class ServletConnexion
  */
+@WebServlet("/ServletConnexion")
 public class ServletConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final static Log LOG = LogFactory.getLog(ServletConnexion.class);
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -39,8 +50,39 @@ public class ServletConnexion extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		if (ServletConnexion.LOG.isInfoEnabled()) {
+			ServletConnexion.LOG.info("-- Debut -- ");
+		}
+		WebApplicationContext appContext = WebApplicationContextUtils
+				.getWebApplicationContext(request.getServletContext());
 		String login = request.getParameter("inLogin");
 		String pwd = request.getParameter("inPass");
+
+		if ((login != null) && (pwd != null)) {
+
+			try {
+				IAuthentificationService serviceAuth = appContext.getBean("AuthentificationService",
+						IAuthentificationService.class);
+				IUtilisateurEntity utilisateur = serviceAuth.authentifier("dj", "dj");
+				// if (utilisateur != null) {
+				request.getSession(true).setAttribute("loginUser", login);
+				request.getSession(true).setAttribute("pwdUser", pwd);
+				// }
+
+				if (ServletConnexion.LOG.isInfoEnabled()) {
+					ServletConnexion.LOG.info("Bonjour " + utilisateur.getNom() + " " + utilisateur.getPrenom());
+				}
+
+			} catch (Throwable e) {
+				ServletConnexion.LOG.fatal("Erreur", e);
+				System.exit(-1);
+			}
+			if (ServletConnexion.LOG.isInfoEnabled()) {
+				ServletConnexion.LOG.info("-- Fin -- ");
+			}
+			request.getRequestDispatcher("map.html").forward(request, response);
+			// System.exit(0);
+		}
 	}
 
 }
