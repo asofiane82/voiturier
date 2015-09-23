16caa0f7f6d4c1ced9df39ee2f8c5c8fb39dad1f
Aston <Aston@Aston-Pc>
2015-09-23 11:35:24 +0200

    ok

--- Voiturier/src/main/java/com/voiturier/dao/IUtilisateurDao.java
+++ Voiturier/src/main/java/com/voiturier/dao/IUtilisateurDao.java
@@ -1,12 +1,17 @@
 /**
- * 
+ *
  */
 package com.voiturier.dao;
+
+import com.voiturier.dao.ex.ExceptionDao;
+import com.voiturier.entity.IUtilisateurEntity;
 
 /**
  * @author Aston
  *
  */
-public interface IUtilisateurDao {
+public interface IUtilisateurDao extends IDAO<IUtilisateurEntity> {
+
+	public abstract IUtilisateurEntity selectLogin(String pLogin) throws ExceptionDao;
 
 }
